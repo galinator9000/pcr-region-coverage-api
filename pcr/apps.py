36bc4f9c6ccefe2cfe566e2dcca06602bb823564
Gali <gali.nil.un@gmail.com>
2025-08-20 23:01:37 +0300

    feat(rest): Django rest framework initialized

--- pcr/apps.py
+++ pcr/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PcrConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pcr'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pcr"
