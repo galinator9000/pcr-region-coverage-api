9343df0c5d68c1e8ff37c3da99b115a5f33c5d43
Gali <gali.nil.un@gmail.com>
2025-08-20 23:28:18 +0300

    feat(gene): Custom pagination feature added to the endpoint

--- pcr/views.py
+++ pcr/views.py
@@ -1,9 +1,32 @@
 from django.http import HttpResponse
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 from .models import Gene
 from .serializers import GeneSerializer
+
+
+class CustomGenePagination(PageNumberPagination):
+    page_size_query_param = "pagesize"
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "items": data,
+                "pagination": {
+                    "current_item_count": len(data),
+                    "current_page_number": self.page.number,
+                    "pagination_size": self.page_size,
+                    "total_item_count": self.page.paginator.count,
+                    "total_page_count": self.page.paginator.num_pages,
+                },
+            }
+        )
 
 
 class GeneViewSet(viewsets.ModelViewSet):
     queryset = Gene.objects.all()
     serializer_class = GeneSerializer
+    pagination_class = CustomGenePagination
